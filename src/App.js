a84e2dfff0848b204aee536778138de2bfff02c9
Carlos A. Herrera <cherrera1208@gmail.com>
2022-08-25 16:53:13 -0700

    lab07-task04, complete

--- src/App.js
+++ src/App.js
@@ -10,24 +10,37 @@ class App extends React.Component {
     this.state = {
       city: '',
       cityData: {},
+      weatherData: [],
       mapURL: '',
+      weatherInfo: '',
       error: false,
-      errorMsg: ''
+      errorMsg: '',
+      displayForecast: '',
     };
   }
-
   handleCitySubmit = async (e) => {
     e.preventDefault();
     try {
       let response = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_CITY_KEY}&q=${this.state.city}&format=json`);
       console.log(response.data[0]);
 
+      let forecast = await axios.get(`${process.env.REACT_APP_SERVER}/weather?city_name=${this.state.city}`)
+      let displayForecast = this.state.weatherData.map((data, idx) => {
+        return (
+          <div>
+            {data.description},
+            {data.date},
+          </div>
+        )
+      });
+      console.log(forecast.data);
+      console.log(this.state.weatherData);
+
       this.setState({
-        /*
-          reset the state of the error to false to toggle the page from one state (show map) to the other (show error)
-          */
         error: false,
         cityData: response.data[0],
+        weatherData: forecast.data.queriedCity,
+        displayForecast: displayForecast[0],
         mapURL: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_KEY}&center=${response.data[0].lat},${response.data[0].lon}&zoom=12`
       })
     } catch (error) {
@@ -35,9 +48,10 @@ class App extends React.Component {
       console.log(this.state.errorMsg);
       this.setState({
         error: true,
-        errorMsg: `an error occurred ${error.response.status}`,
+        errorMsg: `An error occurred: ${error.message}`,
       })
     }
+    console.log(this.state.displayForecast);
   }
   handleCityInput = (e) => {
     let city = e.target.value;
@@ -63,34 +77,20 @@ class App extends React.Component {
         {this.state.error ? (
           <p>{this.state.errorMsg}</p>
         ) :
-          // <>
-          //   <img src={this.state.mapURL} alt='city map' />
-          //   <p>{this.state.cityData.lat}</p>
-          //   <p>{this.state.cityData.lon}</p>
-          // </>
-
 
           this.state.cityData.lat && !this.state.error
             ?
             <>
               <img src={this.state.mapURL} alt='city map' />
-              <p>{this.state.cityData.lat}</p>
-              <p>{this.state.cityData.lon}</p>
+              <p>Latitude: {this.state.cityData.lat}</p>
+              <p>Longitude: {this.state.cityData.lon}</p>
+              {this.state.displayForecast}
             </>
             :
             <>
               <p>{this.state.errorMsg}</p>
             </>
         }
-        {/* {
-          this.state.error
-            ?
-            <p>{this.state.errorMsg}</p>
-            :
-            <p>
-              {this.state.mapURL}
-            </p>
-        } */}
       </>
     )
   }
